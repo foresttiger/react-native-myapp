71bbeec25d815d292749cf6cfb162406af8ce6ea
慢行的小虎 <821991758@qq.com>
2017-09-25 00:37:23 +0800

    no message

--- myApp/js/Root.js
+++ myApp/js/Root.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import {AppRegistry} from 'react-native'
 import { Provider } from 'react-redux';
 import configureStore from './store/ConfigureStore';
+import Orientation from 'react-native-orientation';
 
 import App from './container/App';
 
 const store = configureStore();
 
 export default class myApp extends Component {
+	componentWillMount() {
+		/*锁定只允许横屏*/
+	    // Orientation.lockToLandscape();
+	    /*锁定只允许竖屏*/
+	    Orientation.lockToPortrait();
+	}
   render() {
     return (
       <Provider store={store}>
